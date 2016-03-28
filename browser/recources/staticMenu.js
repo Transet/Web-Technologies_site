fa700b82d40b0ea918711ab35df573fa37e266e1
Transet <Transet.54@mail.ru>
2016-03-28 10:57:04 +0300

    Extra credit: add event throttling for scroll event handler. #14

--- browser/recources/staticMenu.js
+++ browser/recources/staticMenu.js
@@ -1,4 +1,27 @@
-﻿window.addEventListener("scroll", function() {
+﻿/**
+ * Throttle funtion:
+ * throttle(callback, limit, [options]) 
+ * time in milliseconds
+ * example:
+ * var throttled = throttle(updatePosition, 100, {leading: false});
+ * {leading: false} - to disable the first call
+ */
+function throttle (callback, limit, options) {
+	var wait = false;
+    if( options ) {
+		wait = ((options.leading) ? true : false);
+	}
+    return function () {
+        if (!wait) {
+            callback.call();
+            wait = true;
+            setTimeout(function () {
+                wait = false;
+            }, limit);
+        }
+    }
+};
+makeMenuFixed = function() {
 	var leftToolbar = document.getElementById("leftToolbar");
 	var headerImage = document.getElementById("headerImage");
 	if(leftToolbar === null || headerImage === null ) {
@@ -9,4 +32,7 @@
 	} else if( window.scrollY <= headerImage.getBoundingClientRect().height && leftToolbar.style.position !== "") {
 		leftToolbar.style.position = "";
 	}
-});
+};
+var throttledScroll = throttle(makeMenuFixed, 100 , {leading: false});
+
+window.addEventListener("scroll", throttledScroll);
